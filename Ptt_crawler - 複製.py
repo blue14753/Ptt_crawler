9b823a06b575631d3336ce942007d10ba4225454
blue14753 <blue14753@gmail.com>
2019-11-06 21:08:19 +0800

    parse article content

--- Ptt_crawler - 複製.py
+++ Ptt_crawler - 複製.py
@@ -17,14 +17,13 @@ def parse_article(url):
         date = art.find('div',class_='date').text
         try:
             content_url = art.find('a').get('href')
-            content,comments = parse_content('https://www.ptt.cc/'+content_url)
         except AttributeError:
             print("error " + str(title.find('a')) )
-
         articles['title'].append(title)
-        articles['attr'].append( {'point':point,'author':author,'date':date,'content_url':'https://www.ptt.cc/'+content_url,'content':content,'comments':comments} )
+        articles['attr'].append({'point':point,'author':author,'date':date,'content_url':'https://www.ptt.cc/'+content_url})
+        #articles.append({ 'title':title,'attr':{'point':point,'author':author,'date':date,'content_url':'https://www.ptt.cc/'+content_url} })
     json_data = json.dumps(articles,ensure_ascii=False)
-    print(json_data)
+    #print(json_data)
     
     fp = codecs.open("data.json", 'w', encoding="utf-8")
     json.dump(json_data, fp, indent=4, ensure_ascii=False)
@@ -36,6 +35,7 @@ def parse_content(url):
     req = requests.get(url).content.decode('utf-8')
     soup = BeautifulSoup(req,'html.parser')
 
+    article_content = {'title':[],'attr':[]}
     comments = []
     try:
         with open("data.json",'r',encoding='utf-8') as fp:
@@ -53,7 +53,16 @@ def parse_content(url):
         push_ipdatetime = com.find('span',class_='push-ipdatetime').text
         comments.append( {'push_tag':push_tag,'push_userid':push_userid,'push_content':push_content,'push_ipdatetime':push_ipdatetime} )
 
-    return content,comments
+    article_content['title'] = title
+    article_content['attr'] = {'content':content,'comment':comments}
+
+    json_tmp = json.dumps(article_content,ensure_ascii=False)
+    json_data = merge_dict(json_tmp,)
+
+    fp = codecs.open("content_data.json", 'w', encoding="utf-8")
+    json.dump(json_data, fp, indent=4, ensure_ascii=False)
+
+    return json_data
 
 """
 def merge_dict(*dicts):
@@ -67,15 +76,33 @@ def merge_dict(*dicts):
     return merged_dict
 """
 
+def merge_dict(*dicts):
+    merged_dict = {}
+    for d in dicts:
+        for key in d:
+            try:
+                merged_dict[key].append(d[key])
+            except KeyError:
+                merged_dict[key] = [d[key]]
+    return merged_dict
+
+
+
+    
+
+
+
     
 
 
 url = 'https://www.ptt.cc/bbs/joke/index.html'
 url2 = 'https://www.ptt.cc/bbs/joke/M.1573012478.A.58E.html'
 
-parse_article(url)
+#parse_article(url)
 #parse_content(url2)
 
 a = { 'title':['123','456'],'attr':[{'author':'apple'},{'author':'banana'}] }
 b = {'title':['123','456','789'],'attr':[{'author':'apple2'},{'author':'banana2'}]}
+
+print(merge_dict(a,b))
 
